64c870a634086e086e7a4e06845facd004a132cc
Lazarus <lazaruss2024@gmail.com>
2025-02-20 13:41:14 +0700

    fouth stage

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,14 +9,14 @@ const Footer = () => {
       <footer className="py-8 border-t">
          <div className="container mx-auto">
             <div className="flex flex-col items-center justify-between md:flex-row">
-               <p className="text-sm text-muted-foreground">
+               <p className="ml-6 text-sm text-muted-foreground">
                 © {currentYear} Khant Lin Thein. All rights reserved.
                </p>
 
-               <div className="flex mt-4 space-x-4 md:mt-0">
-                  <Link href="https:/pornhub.com" target="_blank" rel="noopener noreferrer" className="transition-colors text-muted-foreground hover:text-foreground">GitHub</Link>
-                  <Link href="https:/pornhub.com" target="_blank" rel="noopener noreferrer" className="transition-colors text-muted-foreground hover:text-foreground">LinkedIn</Link>
-                  <Link href="https:/pornhub.com" target="_blank" rel="noopener noreferrer" className="transition-colors text-muted-foreground hover:text-foreground">Twitter</Link>
+               <div className="flex mt-4 mr-6 space-x-4 md:mt-0">
+                  <Link href="https://github.com/khantlinthein-devmm" target="_blank" rel="noopener noreferrer" className="transition-colors text-muted-foreground hover:text-foreground">GitHub</Link>
+                  <Link href="https://www.linkedin.com/in/khant-linn-thein-5b3658351/" target="_blank" rel="noopener noreferrer" className="transition-colors text-muted-foreground hover:text-foreground">LinkedIn</Link>
+                  
                </div>
             </div>
          </div>
